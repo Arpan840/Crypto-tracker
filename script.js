803bfcd9f02a1762456c6ef7793b39120dacca68
Arpan Das <arpandas020498@gmail.com>
2023-07-30 13:00:28 +0530

    list view styling changed to table

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const navbar = document.querySelector(".navbar-light");
   const coinBoxes = document.getElementsByClassName("coinBox");
   const data = document.querySelector(".data");
-
+  let coinDetails;
+  let originalGridViewHTML;
   function toggle() {
     if (toggleButton.innerHTML.includes("fa-toggle-off")) {
       toggleButton.innerHTML =
@@ -26,32 +27,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
   toggleButton.addEventListener("click", toggle);
 
+  toggleButton.addEventListener("click", toggle);
+
   viewGrid.style.borderBottom = "2px solid blue";
 
   function switchToGridView() {
     viewGrid.style.borderBottom = "2px solid blue";
     viewList.style.borderBottom = "";
-    for (const coinBox of coinBoxes) {
-      coinBox.style.width = "18rem";
-      coinBox.style.flexDirection = "column";
-      coinBox.style.gap = "";
-      coinBox.style.paddingRight = ''
-      coinBox.style.justifyContent = "";
-    }
-    data.classList.remove("list-view");
+
+    // Restore original grid view HTML
+    data.innerHTML = originalGridViewHTML;
   }
 
   function switchToListView() {
     viewGrid.style.borderBottom = "";
     viewList.style.borderBottom = "2px solid blue";
-    for (const coinBox of coinBoxes) {
-      coinBox.style.width = "95%";
-      coinBox.style.justifyContent = "center";
-      coinBox.style.flexDirection = "row";
-      coinBox.style.gap = "2%";
-      coinBox.style.paddingRight = '10%'
-    }
-    data.classList.add("list-view");
+
+    let table = `
+      <table class="coinTable">
+        <tr>
+          <th>Crypto Image</th>
+          <th>ID</th>
+          <th>Name</th>
+          <th>24h % Change</th>
+          <th>Price (USD)</th>
+          <th>Total Volume</th>
+          <th>Market Cap</th>
+        </tr>
+    `;
+
+    coinDetails.forEach((i) => {
+      table += `
+        <tr>
+          <td><img src=${i.image} alt="bitcoin" class="cryptoImage" /></td>
+          <td>${i.id}</td>
+          <td>${i.name}</td>
+          <td>${i.price_change_percentage_24h}%</td>
+          <td>$${i.current_price}</td>
+          <td>${i.total_volume}</td>
+          <td>${i.market_cap}</td>
+        </tr>
+      `;
+    });
+
+    table += `</table>`;
+    data.innerHTML = table;
   }
 
   viewGrid.addEventListener("click", switchToGridView);
@@ -62,7 +82,8 @@ document.addEventListener("DOMContentLoaded", () => {
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
     );
 
-    let coinDetails = await response.json();
+    coinDetails = await response.json(); // Assign fetched data to coinDetails
+
     let coins = "";
 
     coinDetails.forEach((i) => {
@@ -82,6 +103,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     data.innerHTML = coins;
+
+    // Store the original grid view HTML after fetching data
+    originalGridViewHTML = coins;
   }
 
   cryptoData();
